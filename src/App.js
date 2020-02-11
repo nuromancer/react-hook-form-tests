e95c0c5969036d43217d5cfe61b5d25ef5b62545
Aron Binienda <nuromancer.github@gmail.com>
2020-02-11 15:50:59 +0100

    added nice styles and different app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { useForm } from 'react-hook-form';
+import './styles.css';
 
-export default function App() {
-  const { register, handleSubmit } = useForm()
+const App = () => {
+  const onSubmit = data => {
+    alert(JSON.stringify(data));
+  };
 
-  const logSubmit = data => console.log(data)
+  const { register, handleSubmit } = useForm();
 
   return (
-    <form onSubmit={handleSubmit(logSubmit)}>
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+      <h1>Sign Up</h1>
+      <label>First Name:</label>
       <input name="firstName" ref={register} />
-      <select name="gender" ref={register}>
-        <option value="male">male</option>
-        <option value="female">female</option>
+
+      <label>Last Name:</label>
+      <input name="firstName" ref={register} />
+
+      <label>Gender</label>
+      <select name="gender">
+        <option>Select...</option>
+        <option>Male</option>
+        <option>Female</option>
       </select>
-      <input type="submit" />
+
+      <label>Username</label>
+      <input name="username" ref={register} />
+
+      <label>Email</label>
+      <input name="email" ref={register} />
+
+      <label>About you</label>
+      <textarea name="about you" />
+
+      <input type="submit" ref={register} />
     </form>
-  )
-}
+  );
+};
+
+export default App;
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<App />, rootElement);
